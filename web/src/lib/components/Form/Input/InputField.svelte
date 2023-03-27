59a28aed7c4b0eac5f7fd053d5cbb0d9d9655b50
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-27 20:15:19 -0300

    feat: tweak Input hover

--- web/src/lib/components/Form/Input/InputField.svelte
+++ web/src/lib/components/Form/Input/InputField.svelte
@@ -9,10 +9,6 @@
 		font-size: var(--font-size-body-normal);
 		transition: color 0.25s ease-in-out;
 
-		&:hover {
-			color: var(--color-purple-800);
-		}
-
 		&:focus-within {
 			color: var(--color-purple-600);
 		}
