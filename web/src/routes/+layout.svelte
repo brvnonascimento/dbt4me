9dc51e4e8a5c2504172666c34a679766a82fe07d
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-28 00:45:10 -0300

    feat: force reload on change of locale links

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -5,12 +5,6 @@
 
 	import { page } from '$app/stores';
 	import { handleSession } from '@lucia-auth/sveltekit/client';
-	import { setLocale } from '$i18n/i18n-svelte';
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-	// at the very top, set the locale before you access the store and before the actual rendering takes place
-	setLocale(data.locale);
 
 	handleSession(page);
 </script>
@@ -21,8 +15,8 @@
 </svelte:head>
 
 <nav>
-	<a href={`/`}>en</a>
-	<a href={`/pt-BR`}>pt-BR</a>
+	<a href={`/en`} data-sveltekit-reload>en</a>
+	<a href={`/pt-BR`} data-sveltekit-reload> pt-BR</a>
 </nav>
 
 <!-- <header>
